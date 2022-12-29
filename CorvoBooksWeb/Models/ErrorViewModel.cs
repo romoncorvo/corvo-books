4231291b4693586eddf6f5bc3f24126c52316a6e
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2022-12-29 10:26:12 +0100

    Add project files.

---
+++ CorvoBooksWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CorvoBooksWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
